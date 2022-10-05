0b004bcae1bd4843aef5b516df6cf6e5dfb6da3a
23yimichael <23yimichael@gmail.com>
2022-10-05 00:04:02 -0700

    last bio update

--- web/components/About.tsx
+++ web/components/About.tsx
@@ -6,18 +6,17 @@ const About = () => {
         Hwanjun (Michael) Yi is a senior at Portola High School in Irvine,
         California. Born in Seoul, South Korea, he is passionate about using the
         intersection of technology, healthcare, and entrepreneurship to
-        evolutionize industries and connect others. He aspires to change the
+        revolutionize industries and connect others. He aspires to change the
         world through leadership and innovation, and his journey in doing so
         will be documented throughout this website. <br />
         <br />
         In his free time, he plays baseball and is extremely passionate about
         music and cinema. He has played the violin for 8 years and recently
-        picked up the guitar this year. He loves listening to music; his
-        favorite bands are The Beatles, The Velvet Underground, King Crimson,
-        etc. His favorite films are 2001: A Space Odyssey, Come and See, and No
-        Country For Old Men. Lastly, his favorite pastimes are eating acai bowls
-        and hiking a nearby mountain to practice guitar at the top with his
-        friends.
+        picked up the guitar this year. His favorite bands are The Beatles, The
+        Velvet Underground, and King Crimson; his favorite films are 2001: A
+        Space Odyssey, Come and See, and No Country For Old Men. Lastly, his
+        favorite pastimes are eating acai bowls and hiking to the top of a
+        nearby mountain to practice guitar with his friends.
       </div>
     </div>
   );
