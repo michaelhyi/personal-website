80b38be7ee194de2d697231aa3d8fbb6a9ec9099
23yimichael <23yimichael@gmail.com>
2022-10-04 22:34:43 -0700

    projects section reponsiveness bugs fixeD

--- web/components/Project.tsx
+++ web/components/Project.tsx
@@ -14,13 +14,22 @@ interface Props {
 
 const Project: React.FC<Props> = ({ key, data }) => {
   return (
-    <div className="flex flex-col items-center text-center bg-zinc-800 sm:h-[450px] sm:w-[450px] md:h-[500px] md:w-[500px] rounded-3xl w-full">
-      <div className="font-semibold text-4xl mt-12">{data.name}</div>
-      <div className="mt-4 text-sm w-96">{data.desc}</div>
-      <div className="flex h-[200px] w-[200px] rounded-xl bg-white items-center justify-center mt-8">
-        {data.logo && <img src={data.logo} className="h-[150px] w-[150px]" />}
+    <div className="flex flex-col items-center text-center bg-zinc-800 sm:h-[350px] sm:w-[350px] md:h-[500px] md:w-[500px] rounded-3xl w-full">
+      <div className="font-semibold sm:text-2xl md:text-4xl sm:mt-8 md:mt-12">
+        {data.name}
       </div>
-      <div className="font-medium text-sm sm:mt-6 mt-12">
+      <div className="mt-4 sm:text-xs sm:w-80 md:text-sm md:w-96">
+        {data.desc}
+      </div>
+      <div className="flex sm:h-[150px] sm:w-[150px] md:h-[200px] md:w-[200px] rounded-xl bg-white items-center justify-center mt-8">
+        {data.logo && (
+          <img
+            src={data.logo}
+            className="sm:h-[112.5px] sm:w-[112.5px] md:h-[150px] md:w-[150px]"
+          />
+        )}
+      </div>
+      <div className="font-medium text-sm sm:mt-4 mt-12">
         {data.video && (
           <a
             className="text-blue-400 hover:cursor-pointer duration-300 hover:opacity-50"
