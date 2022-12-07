2d6a5a72ed358420f653a232c0989ad98e1d550b
23yimichael <23yimichael@gmail.com>
2022-12-07 04:45:37 -0800

    added analytics

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Helmet from "../components/Helmet";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
       <Helmet />
+      <Analytics />
     </ChakraProvider>
   );
 }
