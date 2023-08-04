d2ec1153ac8e27740d41b20005c1e9a0877701ed
michaelhyi <michaelyi2004@gmail.com>
2023-08-04 11:13:55 -0700

    feat(font): added unviversal font [MYC-6] (#20)

--- web/src/providers/index.tsx
+++ web/src/providers/index.tsx
@@ -1,8 +1,24 @@
+import localFont from "next/font/local";
 import { ReactNode, useEffect, useState } from "react";
 
 interface Props {
   children: ReactNode;
 }
+
+const font = localFont({
+  src: [
+    {
+      path: "../../public/fonts/SF-Pro-Text-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/SF-Pro-Text-Bold.otf",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+});
 
 const Providers: React.FC<Props> = ({ children }) => {
   const [mounted, setMounted] = useState(false);
@@ -13,7 +29,7 @@ const Providers: React.FC<Props> = ({ children }) => {
 
   if (!mounted) return <></>;
 
-  return <>{children}</>;
+  return <main className={font.className}>{children}</main>;
 };
 
 export default Providers;
