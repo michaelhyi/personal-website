49cdbe591ca0d23fb4793fa7d5d31639b25b4559
michaelhyi <michaelyi2004@gmail.com>
2023-08-08 12:16:27 -0700

    feat(404): updated 404 not found page [MYC-13] (#25)

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -2,7 +2,7 @@ const NotFound = () => {
   return (
     <div>
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xs">
-        Not found.
+        404, not found.
       </div>
       <div className="absolute bottom-4 text-xs left-0 right-0 text-center">
         &copy; 2023 Michael Yi, All Rights Reserved.
