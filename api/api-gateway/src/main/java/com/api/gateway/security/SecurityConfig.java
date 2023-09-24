bc19d7af9076c5b76e81904c05f6480962c41a53
michaelhyi <michaelyi2004@gmail.com>
2023-09-24 03:28:06 -0400

    fix(cors): fixed cors error (#37) [MYC-62]

--- api/api-gateway/src/main/java/com/api/gateway/security/SecurityConfig.java
+++ api/api-gateway/src/main/java/com/api/gateway/security/SecurityConfig.java
@@ -1,18 +1,32 @@
 package com.api.gateway.security;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
+    @Value("${security.cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Value("${security.cors.allowed-headers}")
+    private String allowedHeaders;
+
+    @Value("${security.cors.allowed-methods}")
+    private String allowedMethods;
 
     private AuthenticationManager authenticationManager;
     private SecurityContextRepository securityContextRepository;
@@ -24,6 +38,32 @@ public class SecurityConfig {
         this.authenticationManager = authenticationManager;
         this.securityContextRepository = securityContextRepository;
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader(allowedHeaders);
+        configuration.addAllowedOrigin(allowedOrigins);
+        configuration.addAllowedMethod(allowedMethods);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+        return urlBasedCorsConfigurationSource;
+    }
+
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader(allowedHeaders);
+        corsConfiguration.addAllowedMethod(allowedMethods);
+        corsConfiguration.addAllowedOrigin(allowedOrigins);
+        corsConfiguration.addExposedHeader(HttpHeaders.SET_COOKIE);
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(corsConfigurationSource);
+    }
+
 
     @Bean
     public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http) {
@@ -42,6 +82,7 @@ public class SecurityConfig {
                         .anyExchange()
                         .authenticated()
                 )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .formLogin(formLogin -> formLogin.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
