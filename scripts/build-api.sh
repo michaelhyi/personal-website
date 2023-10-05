73c097a88f9ea4268b05aeff633a3f115f316734
michaelhyi <michaelyi2004@gmail.com>
2023-10-05 15:48:21 -0400

    chore(ci/cd): established separate deployment and pr actions

--- scripts/build-api.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo What should the version be?
-read VERSION
-
-cd ../api && 
-docker build -t michaelyi/personal-website-api:$VERSION . &&
-docker push michaelyi/personal-website-api:$VERSION
