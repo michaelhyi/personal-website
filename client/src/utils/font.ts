e539b6cdd5a5eddccf8f6ad44a3b829568e0eac9
michaelhyi <michaelyi2004@gmail.com>
2023-10-25 16:00:26 -0400

    fix(fonts): removed helvetica

--- client/src/utils/font.ts
+++ client/src/utils/font.ts
@@ -3,15 +3,15 @@ import localFont from "next/font/local";
 
 export const font = Inter({ subsets: ["latin"] });
 
-export const _font = localFont({
-   src: [
-      {
-         path: "../../public/fonts/HelveticaNeue-Light.otf",
-         weight: "300",
-      },
-      {
-         path: "../../public/fonts/HelveticaNeue-Regular.otf",
-         weight: "400",
-      },
-   ],
-});
+// export const _font = localFont({
+//    src: [
+//       {
+//          path: "../../public/fonts/HelveticaNeue-Light.otf",
+//          weight: "300",
+//       },
+//       {
+//          path: "../../public/fonts/HelveticaNeue-Regular.otf",
+//          weight: "400",
+//       },
+//    ],
+// });
