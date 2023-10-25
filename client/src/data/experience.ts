b6c5d1bc350cfd136a3ca9b46a31f2ff05cd4785
michaelhyi <michaelyi2004@gmail.com>
2023-10-25 17:19:28 -0400

    fix(experience): fixed experience description

--- client/src/data/experience.ts
+++ client/src/data/experience.ts
@@ -1,31 +1,19 @@
 export const experience = [
-   // {
-   //    company: "Fintech @ Georgia Tech",
-   //    position: "Researcher",
-   //    href: "https://fintechgt.substack.com/p/fast-track-1",
-   //    date: "2023",
-   // },
    {
       company: "Ardent Labs",
       position: "Software Engineer Intern",
       href: "https://www.ardentlabs.io/",
       date: "2023",
       description:
-         " Spearheaded strategic overhaul of backend services by migrating production databases from RethinkDB to PostgreSQL under a microservices architecture consisting of 100+ services and 400k+ data documents.",
+         "Spearheaded strategic overhaul of backend services by migrating production databases from RethinkDB to PostgreSQL under a microservices architecture.",
    },
-   // {
-   //    company: "health{hacks}",
-   //    position: "Executive Director",
-   //    href: "https://www.joinhealthhacks.com/",
-   //    date: "2022 - 2023",
-   // },
    {
       company: "MegaEvolution",
       position: "Software Engineer Intern",
       href: "https://megaevolution.io/",
       date: "2022 - 2023",
       description:
-         "Reinforced financial infrastructure of Web3 marketing platform through seamless implementation of credit card and cryptocurrency transactions using PayPal and Venmo integration, now serving 200+ content creators.",
+         "Reinforced financial infrastructure of Web3 marketing platform through seamless implementation of credit card and cryptocurrency transactions.",
    },
    {
       company: "USC INK Lab",
@@ -33,6 +21,6 @@ export const experience = [
       href: "https://inklab.usc.edu/",
       date: "2022",
       description:
-         "Researched and enhanced generative commonsense reasoning AI models by combining natural language processing and computer vision to generate English sentences when given image and keyword input using Python and PyTorch.",
+         "Enhanced generative commonsense reasoning AI models by combining natural language processing and computer vision to generate English sentences when given image and keyword input.",
    },
 ];
