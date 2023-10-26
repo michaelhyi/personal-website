8e0bd2b65dbf23d56b246bfbd6d415d68e8b7e95
michaelhyi <michaelyi2004@gmail.com>
2023-10-25 20:44:54 -0400

    fix(lint): fixed formatting issues

--- client/src/components/Section.tsx
+++ client/src/components/Section.tsx
@@ -1,32 +1,37 @@
 "use client";
 
-import { FC, ReactNode } from "react";
 import Link from "next/link";
+import { FC, ReactNode } from "react";
 import { FiArrowUpRight } from "react-icons/fi";
 
 interface Props {
    title: string;
    children: ReactNode;
    nonDesc?: boolean;
-   href?: string
+   href?: string;
 }
 
 const Section: FC<Props> = ({ title, children, nonDesc = false, href }) => {
    return (
       <div className="mt-10">
          <div className="font-normal">
-            {href
-               ? <Link href={href}
-                       className="flex
+            {href ? (
+               <Link
+                  href={href}
+                  className="flex
                                   gap-1
                                   font-normal
                                   text-sm
                                   cursor-pointer
                                   duration-500
-                                  hover:opacity-50">
-                  {title}{<FiArrowUpRight size={12} />}
+                                  hover:opacity-50"
+               >
+                  {title}
+                  {<FiArrowUpRight size={12} />}
                </Link>
-               : title}
+            ) : (
+               title
+            )}
          </div>
          <div className={`mt-6 flex flex-col ${nonDesc ? "gap-4" : "gap-8"}`}>
             {children}
