492543721312def17e410c27a9a0c4741baef149
michaelhyi <michaelyi2004@gmail.com>
2023-10-26 12:07:30 -0400

    fix(lint): fixed formatting issues

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { FC } from "react";
 import { icons } from "@/data/icons";
 import Link from "next/link";
+import { FC } from "react";
 
 interface Props {
    absolute?: boolean;
@@ -15,10 +15,19 @@ const Footer: FC<Props> = ({ absolute = false }) => {
             absolute ? "absolute bottom-4 left-0 right-0" : "mt-12 pb-4"
          } items-center gap-3 text-[10px] text-neutral-400`}
       >
-         <div className="flex gap-2">{icons.map(({ icon: Icon, href }) => (
-            <Link key={href} href={href} rel="noopener noreferrer"
-                  target="_blank" className="duration-500 hover:opacity-50"><Icon size={15} /></Link>
-         ))}</div>
+         <div className="flex gap-2">
+            {icons.map(({ icon: Icon, href }) => (
+               <Link
+                  key={href}
+                  href={href}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="duration-500 hover:opacity-50"
+               >
+                  <Icon size={15} />
+               </Link>
+            ))}
+         </div>
 
          <div>&copy; 2023 Michael Yi, All Rights Reserved.</div>
       </div>
