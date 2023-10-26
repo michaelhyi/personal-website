90977fcd59efb299a3e398c8f0a8342b21914391
michaelhyi <michaelyi2004@gmail.com>
2023-10-25 20:44:10 -0400

    fix(resume): fixed resume linking

---
+++ client/src/app/projects/page.tsx
@@ -0,0 +1,22 @@
+import Container from "@/components/Container";
+import Section from "@/components/Section";
+import { projects } from "@/data/projects";
+import Card from "@/components/Card";
+
+const Projects = () => {
+   return <Container>
+      <Section title="Projects">
+         {projects.map(v => <Card
+            key={v.name}
+            href={v.href}
+            desc={v.desc}
+            name={v.name}
+            tech={v.tech}
+            date={v.date}
+            img={v.img}
+         />)}
+      </Section>
+   </Container>;
+};
+
+export default Projects;
--- client/src/components/Section.tsx
+++ client/src/components/Section.tsx
@@ -1,17 +1,33 @@
 "use client";
 
 import { FC, ReactNode } from "react";
+import Link from "next/link";
+import { FiArrowUpRight } from "react-icons/fi";
 
 interface Props {
    title: string;
    children: ReactNode;
    nonDesc?: boolean;
+   href?: string
 }
 
-const Section: FC<Props> = ({ title, children, nonDesc = false }) => {
+const Section: FC<Props> = ({ title, children, nonDesc = false, href }) => {
    return (
       <div className="mt-10">
-         <div className="font-normal">{title}</div>
+         <div className="font-normal">
+            {href
+               ? <Link href={href}
+                       className="flex
+                                  gap-1
+                                  font-normal
+                                  text-sm
+                                  cursor-pointer
+                                  duration-500
+                                  hover:opacity-50">
+                  {title}{<FiArrowUpRight size={12} />}
+               </Link>
+               : title}
+         </div>
          <div className={`mt-6 flex flex-col ${nonDesc ? "gap-4" : "gap-8"}`}>
             {children}
          </div>
