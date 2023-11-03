82ee516fd4d41bf20a3181403ce06168b306d7dc
michaelhyi <michaelyi2004@gmail.com>
2023-11-03 00:58:05 -0400

    refactor(file-naming): enabled case-sensitive naming changes

---
+++ apps/web/src/components/home/Education.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Card from "../Card";
+import Section from "../Section";
+
+const Education = () => {
+  return (
+    <Section title="Education">
+      <Card
+        date="2026"
+        desc="Concentrations: Artificial Intelligence & Information Internetworks"
+        name="B.S. in Computer Science at Georgia Tech"
+      />
+    </Section>
+  );
+};
+
+export default Education;
