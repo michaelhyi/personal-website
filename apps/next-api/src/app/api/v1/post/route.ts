b4dd98ffa380e084e5678e1872c02e882127fccd
Michael Yi <michaelyi2004@gmail.com>
2023-11-09 13:25:30 -0500

    refactor(next-api): removed next-api, migrate to spring api (#134)

--- apps/next-api/src/app/api/v1/post/route.ts
+++
@@ -1,5 +0,0 @@
-import { readAllPosts } from "@/services/post";
-
-export async function GET(_: Request) {
-  return Response.json(await readAllPosts());
-}
--- apps/web/cypress/e2e/navigation.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("Navigation", () => {
-  it("Navigation", () => {
-    cy.visit("http://localhost:3000/");
-    cy.get('a[href*="projects"]').click();
-    cy.url().should("include", "/projects");
-    cy.get("div").contains("Projects");
-  });
-});
