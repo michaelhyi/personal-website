827b0ae47233942b059c87aa3dbcb4a2af6dfa97
michaelhyi <michaelyi2004@gmail.com>
2023-11-09 12:53:51 -0500

    fix(build): fixed prisma build errors

--- apps/web/src/services/post.ts
+++ apps/web/src/services/post.ts
@@ -1,13 +1,14 @@
+import axios from "axios";
 import type Post from "@/types/dto/post";
 
 export const readPost = async (id: string): Promise<Post | null> => {
-  const res = await fetch(`${process.env.API_URL}/post/${id}`);
+  const res = await axios(`${process.env.API_URL}/post/${id}`);
 
-  return res.status === 200 ? ((await res.json()) as Post) : null;
+  return res.status === 200 ? (res.data as Post) : null;
 };
 
 export const readAllPosts = async (): Promise<Post[]> => {
-  const res = await fetch(`${process.env.API_URL}/post`);
+  const res = await axios(`${process.env.API_URL}/post`);
 
-  return (await res.json()) as Post[];
+  return res.data as Post[];
 };
