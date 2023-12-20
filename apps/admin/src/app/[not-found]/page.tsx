fd799b40351f928d3937f5c94fb5f57e9b60217a
michaelhyi <michaelyi2004@gmail.com>
2023-12-20 01:05:03 -0800

    feat(routes): added 404 and loading routes to all pages [PW-131]

---
+++ apps/admin/src/app/[not-found]/page.tsx
@@ -0,0 +1,5 @@
+import { NotFound as NotFoundClient } from "@personal-website/ui";
+
+export default function NotFound() {
+  return <NotFoundClient />;
+}
---
+++ apps/web/src/app/blog/[title]/loading.tsx
@@ -0,0 +1,5 @@
+import { Loading as LoadingClient } from "@personal-website/ui";
+
+export default function Loading() {
+  return <LoadingClient />;
+}
