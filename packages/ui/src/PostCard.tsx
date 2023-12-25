f6462017a33d5e107198acee5f80be7657c2d054
michaelhyi <michaelyi2004@gmail.com>
2023-12-24 20:24:08 -0800

    fix(images): fixed images in next configuration files [PW-147]

--- packages/ui/src/PostCard.tsx
+++ packages/ui/src/PostCard.tsx
@@ -32,7 +32,7 @@ export default function PostCard({
           href={`${process.env.NEXT_PUBLIC_WEB_URL}/blog/${data.title}`}
         >
           <Image
-            unoptimized
+            priority
             className="rounded-lg shadow-md"
             src={readPostImageUrl(data.id)}
             alt={data.title}
