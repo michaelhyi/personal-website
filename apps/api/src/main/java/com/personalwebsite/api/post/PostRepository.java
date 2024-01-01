fd776699aba52c208e05a728515185ee3d762bd1
michaelhyi <michaelyi2004@gmail.com>
2024-01-01 10:42:05 -0800

    refactor(jpa-repository): added transactional annotation at repository level, implemented save calls for entity updates

--- apps/api/src/main/java/com/personalwebsite/api/post/PostRepository.java
+++ apps/api/src/main/java/com/personalwebsite/api/post/PostRepository.java
@@ -1,10 +1,12 @@
 package com.personalwebsite.api.post;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByTitle(String title);
     List<Post> findAllByOrderByDateDesc();
