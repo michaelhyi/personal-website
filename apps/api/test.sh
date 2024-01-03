bc06b4cb95bcf5b0eab14e90c630a1b588bd4a9f
michaelhyi <michaelyi2004@gmail.com>
2024-01-02 20:35:16 -0800

    fix(test): fixed tests not working with actions

--- apps/api/test.sh
+++ apps/api/test.sh
@@ -2,7 +2,7 @@
 
 source ../../.env
 
-mvn clean compile test -Dtest.arguments=" \
+mvn test -Dtest.arguments=" \
 --AWS_ACCESS_KEY=$API_AWS_ACCESS_KEY \
 --AWS_SECRET_KEY=$API_AWS_SECRET_KEY \
 --SECURITY_JWT_SECRET_KEY=$API_SECURITY_JWT_SECRET_KEY
