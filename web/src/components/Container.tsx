d13b0844a2d58c34c79a75d39223157c42eb8939
Michael Yi <michaelyi2004@gmail.com>
2024-01-13 15:17:52 -0800

    Feat/styles (#178)
    * feat(styles): updated blog article text size, set footers to be absolute [PW-156]
    
    * feat(styles): migrate from viewport width to fixed container width [PW-155]

--- web/src/components/Container.tsx
+++ web/src/components/Container.tsx
@@ -1,23 +1,23 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, { type ReactNode } from "react";
+import type { ReactNode } from "react";
 import Footer from "./Footer";
 
 export default function Container({
   children,
   absoluteFooter = false,
-}: {
+}: Readonly<{
   children: ReactNode;
   absoluteFooter?: boolean;
-}) {
+}>) {
   return (
     <div className="bg-black text-white min-h-screen">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.75 }}
-        className="mx-auto flex flex-col pt-20 sm:w-[320px] md:w-[480px] lg:w-[512px] xl:w-1/2"
+        className="mx-auto flex flex-col pt-20 sm:w-[320px] md:w-[480px] lg:w-[512px] xl:w-[768px]"
       >
         {children}
         <Footer absolute={absoluteFooter} />
