0cc1c081f241ac5a4515b666e14273066d292cf7
Michael Yi <michaelyi2004@gmail.com>
2024-01-16 17:35:36 -0500

    feat(styles): cleanaed up styles for PostCard component [PW-154] (#179)

--- admin/src/components/Menu.tsx
+++ admin/src/components/Menu.tsx
@@ -7,12 +7,12 @@ import { IoPencil, IoTrashOutline } from "react-icons/io5";
 export default function Menu({
   id,
   handleToggleModal,
-}: {
+}: Readonly<{
   id: string;
   handleToggleModal: (id?: string | undefined) => void;
-}) {
+}>) {
   return (
-    <div className="z-10 flex flex-col gap-2 absolute rounded-md shadow-lg w-36 bg-black text-white font-medium overflow-hidden right-0 top-5 p-3 border-[1px]">
+    <div className="z-[1000] flex flex-col gap-2 absolute rounded-md shadow-lg w-36 bg-black text-white font-medium overflow-hidden right-0 top-5 p-3 border-[1px]">
       <Link
         href={`/blog?mode=edit&id=${id}`}
         className="flex items-center gap-2 duration:500 hover:opacity-50 text-sm"
--- web/src/components/PostCard.tsx
+++ web/src/components/PostCard.tsx
@@ -1,39 +1,23 @@
 "use client";
 
-import { readPostImageUrl } from "@/services/post";
 import type { Post } from "@/types/post";
 import { format } from "date-fns";
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Hoverable from "@/components/Hoverable";
 
-export default function PostCard({ data }: { data: Post }) {
+export default function PostCard({ data }: Readonly<{ data: Post }>) {
   return (
-    <div className="relative">
-      <Hoverable>
-        <Link
-          className="flex sm:flex-col md:flex-row gap-12"
-          href={`${process.env.NEXT_PUBLIC_WEB_URL}/blog/${data.id}`}
-        >
-          <Image
-            className="rounded-lg shadow-md"
-            src={readPostImageUrl(data.id)}
-            alt={data.title}
-            width={200}
-            height={200}
-          />
-          <div className="w-full">
-            <div className="text-xs font-light text-neutral-500">
-              {format(new Date(data.date), "PPP")}
-            </div>
-            <div className="mt-2 font-bold">{data.title}</div>
-            <div className="mt-2 text-xs text-neutral-400 line-clamp-3">
-              {data.content.replace(/(<([^>]+)>)/gi, "")}
-            </div>
-          </div>
-        </Link>
-      </Hoverable>
-    </div>
+    <Hoverable>
+      <Link
+        className="flex sm:flex-col md:flex-row"
+        href={`${process.env.NEXT_PUBLIC_WEB_URL}/blog/${data.id}`}
+      >
+        <div className="w-48 text-[13px] font-light text-neutral-400">
+          {format(new Date(data.date), "PPP")}
+        </div>
+        <div className="text-sm font-semibold">{data.title}</div>
+      </Link>
+    </Hoverable>
   );
 }
