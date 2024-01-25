80fd52a381c95b4562696fcf2f30e599e61882f8
michaelhyi <michaelyi2004@gmail.com>
2024-01-25 14:50:04 -0500

    refactor(properties): refactored properties for spring boot apps

--- api/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source .env 
-
-mvn test -Dtest.arguments=" \
---AWS_ACCESS_KEY=$API_AWS_ACCESS_KEY \
---AWS_SECRET_KEY=$API_AWS_SECRET_KEY \
---SECURITY_JWT_SECRET_KEY=$API_SECURITY_JWT_SECRET_KEY
-"
