e0e9fca7cfec04bd55ea7d172445dbaa5ebc14d5
michaelhyi <michaelyi2004@gmail.com>
2024-01-24 21:38:30 -0500

    chore(test): finished writing unit tests with JUnit [PW-139]

--- api/src/main/java/com/personalwebsite/api/post/PostRepository.java
+++ api/src/main/java/com/personalwebsite/api/post/PostRepository.java
@@ -3,9 +3,6 @@ package com.personalwebsite.api.post;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 @Transactional
 public interface PostRepository extends JpaRepository<Post, String> {
-    List<Post> findAllByOrderByDateDesc();
 }
