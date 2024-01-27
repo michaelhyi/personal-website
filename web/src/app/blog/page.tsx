e9c7120030c794c01738e2c112792aea9eb90fdd
Michael Yi <michaelyi2004@gmail.com>
2024-01-27 11:44:42 -0500

    feat(header-css): removed headers, updated styles to look cleaner (#195)

--- web/src/app/blog/page.tsx
+++ web/src/app/blog/page.tsx
@@ -12,18 +12,11 @@ export default async function Blog() {
   return (
     <Container absoluteFooter>
       <BackButton href="/" text="Home" />
-      <div className="mt-8">
-        <div className="font-bold text-3xl">Blog</div>
-        <div className="mt-2 text-sm font-medium text-neutral-400">
-          An exploration of my enthusiasm for cinema through reviews, analyses,
-          and essays on various films.
-        </div>
-        <div className="mt-12 flex flex-col gap-2">
+        <div className="mt-10 flex flex-col gap-2">
           {data.map((post) => (
             <PostCard key={post.id} data={post} />
           ))}
         </div>
-      </div>
     </Container>
   );
 }
