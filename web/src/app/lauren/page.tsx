7a538596408de8171d9ac8386495a6d9e7cf0773
Michael Yi <michaelyi2004@gmail.com>
2024-02-18 16:34:07 -0500

    feat(val): reverted val page (#216)

--- web/src/app/lauren/page.tsx
+++ web/src/app/lauren/page.tsx
@@ -2,51 +2,9 @@
 
 import Center from "@/components/Center";
 import Container from "@/components/Container";
-import Hoverable from "@/components/Hoverable";
 import Image from "next/image";
-import { useCallback } from "react";
-import { Toaster, toast } from "react-hot-toast";
 
 export default function Lauren() {
-  const valentine = useCallback(() => {
-    toast.custom(({ visible }) => (
-      <div
-        className={`${visible ? "animate-enter" : "animate-leave"}
-                  bg-green-400
-                  flex
-                  justify-center
-                  shadow-lg
-                  rounded-lg
-                `}
-      >
-        <Image
-          src="/funny-bunny.png"
-          height={200}
-          width={200}
-          alt="funny-bunny"
-        />
-      </div>
-    ));
-  }, []);
-
-  const sad = useCallback(() => {
-    toast.custom(({ visible }) => (
-      <div
-        className={`${visible ? "animate-enter" : "animate-leave"}
-                  bg-red-400
-                  flex
-                  justify-center
-                  shadow-lg
-                  rounded-lg
-                `}
-      >
-        <div className="px-5 py-3 font-semibold text-sm">
-          RAHHHHHHHHHHHHHH 😡😡😡😡😡 🧨🧨🧨🧨🧨 💣💣💣💣💣💣 🔥🔥🔥🔥🔥🔥🔥
-        </div>
-      </div>
-    ));
-  }, []);
-
   return (
     <Container absoluteFooter>
       <Center>
@@ -57,28 +15,9 @@ export default function Lauren() {
             height={250}
             alt="mimi and lala"
           />
-          <div className="text-sm font-light">Will you be my valentine?</div>
-          <div className="flex space-x-6 mt-5">
-            <Hoverable>
-              <button
-                onClick={valentine}
-                className="font-medium bg-green-400 px-4 py-2 rounded-lg shadow-sm"
-              >
-                Yes !!!!
-              </button>
-            </Hoverable>
-            <Hoverable>
-              <button
-                onClick={sad}
-                className="font-medium bg-red-400 px-4 py-2 rounded-lg shadow-sm"
-              >
-                No...
-              </button>
-            </Hoverable>
-          </div>
+          <div className="text-sm font-light">soon...</div>
         </div>
       </Center>
-      <Toaster position="bottom-center" />
     </Container>
   );
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,24 +2,10 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      animation: {
-        enter: "enter 300ms ease-out",
-        leave: "leave 300ms ease-in forwards",
-      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      keyframes: {
-        enter: {
-          "0%": { transform: "scale(0.9)", opacity: 0 },
-          "100%": { transform: "scale(1)", opacity: 1 },
-        },
-        leave: {
-          "0%": { transform: "scale(1)", opacity: 1 },
-          "100%": { transform: "scale(0.9)", opacity: 0 },
-        },
       },
       screens: {
         sm: "320px",
