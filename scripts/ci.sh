98ae006975136c30e8c1070e301be358e7dd8088
michaelhyi <michaelyi2004@gmail.com>
2024-02-22 19:41:59 -0500

    refactor(docker-compose): migrated from docker-compose

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,16 +2,15 @@
 
 cd ../
 rm -rf node_modules
-docker compose up -d
 yarn install
 
 cd admin
-rm -rf .next node_modules
+rm -rf build coverage node_modules
 yarn install
 
 cd ../api
 mvn clean
 
 cd ../web 
-rm -rf .next node_modules
+rm -rf build coverage node_modules
 yarn install
