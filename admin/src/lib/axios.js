cdb856d630da4a997e7222c806137c487aa28c0a
michaelhyi <michaelyi2004@gmail.com>
2024-02-26 19:35:31 -0500

    fix(lint): fixed linting issues with web and admin ci

--- admin/src/lib/axios.js
+++ admin/src/lib/axios.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
 import { setupCache } from "axios-cache-interceptor";
 
-const instance = Axios.create(); 
+const instance = Axios.create();
 export default setupCache(instance);
