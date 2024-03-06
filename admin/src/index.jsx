c2757fd76fbe4147576a50a7d42cad593d6b2581
michaelhyi <michaelyi2004@gmail.com>
2024-03-05 19:50:09 -0500

    chore(react17): migrated to react v17 [PW-201]

--- admin/src/index.jsx
+++ admin/src/index.jsx
@@ -1,7 +1,8 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { inject } from "@vercel/analytics";
 import React from "react";
-import ReactDOM from "react-dom/client";
+// eslint-disable-next-line react/no-deprecated
+import { render } from "react-dom";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -19,17 +20,14 @@ const router = createBrowserRouter([
 const GOOGLE_CLIENT_ID =
     "148503656040-qu62io184ardtqdmmb6ph2ch8p466e2c.apps.googleusercontent.com";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+render(
     <React.StrictMode>
         <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
             <RouterProvider router={router} />
         </GoogleOAuthProvider>
     </React.StrictMode>,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 inject();
