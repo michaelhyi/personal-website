f31a6ac1b1da646205daf6418976d1208104bd7c
Michael Yi <michaelyi2004@gmail.com>
2024-03-05 19:58:07 -0500

    chore(react17): migrated to react v17 [PW-201] (#266)
    * chore(react17): migrated to react v17 [PW-201]
    
    * fix(lint): fixed prettier check fails in ci

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,6 +1,7 @@
 import { inject } from "@vercel/analytics";
 import React from "react";
-import ReactDOM from "react-dom/client";
+// eslint-disable-next-line react/no-deprecated
+import { render } from "react-dom";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -39,15 +40,12 @@ const router = createBrowserRouter([
     },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+render(
     <React.StrictMode>
         <RouterProvider router={router} />
     </React.StrictMode>,
+    document.getElementById("root"),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 inject();
