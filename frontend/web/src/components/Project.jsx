d73911871df3f7008c2d9d62393357edde160af6
Michael Yi <michaelyi2004@gmail.com>
2024-03-31 16:42:40 -0400

    refactor(projects): refactored projects section of portfolio [PW-235] (#308)

---
+++ frontend/web/src/components/Project.jsx
@@ -0,0 +1,36 @@
+import Hoverable from "./Hoverable";
+
+export default function Project({ project }) {
+    return (
+        <Hoverable key={project.name}>
+            <a rel="noopener noreferrer" target="_blank" href={project.href}>
+                <div className="flex sm:flex-col md:flex-row">
+                    <div className="w-56 text-[13px] font-light text-neutral-400 sm:mb-2 md:mb-0">
+                        {project.date}
+                    </div>
+                    <div className="w-full">
+                        <div className="flex gap-1 font-semibold">
+                            {project.name}
+                        </div>
+                        <div className="mt-1 text-xs text-neutral-400">
+                            {project.description}
+                        </div>
+                        <div className="mt-2 text-[10px] text-neutral-300 font-light">
+                            {project.tech}
+                        </div>
+                        {project.image !== undefined &&
+                            project.image.length !== 0 && (
+                                <img
+                                    className="mt-4 rounded-md shadow-md"
+                                    src={project.image}
+                                    alt={project.name}
+                                    width={200}
+                                    height={120}
+                                />
+                            )}
+                    </div>
+                </div>
+            </a>
+        </Hoverable>
+    );
+}
--- frontend/web/src/pages/portfolio.jsx
+++ frontend/web/src/pages/portfolio.jsx
@@ -1,6 +1,6 @@
 import BackButton from "../components/BackButton";
 import Container from "../components/Container";
-import Hoverable from "../components/Hoverable";
+import Project from "../components/Project";
 import PROJECTS from "../constants/projects";
 
 export default function Home() {
@@ -9,40 +9,7 @@ export default function Home() {
             <BackButton href="/" text="Home" />
             <div className="mt-10 flex flex-col gap-8">
                 {PROJECTS.map((project) => (
-                    <Hoverable key={project.name}>
-                        <a
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            href={project.href}
-                        >
-                            <div className="flex sm:flex-col md:flex-row">
-                                <div className="w-56 text-[13px] font-light text-neutral-400 sm:mb-2 md:mb-0">
-                                    {project.date}
-                                </div>
-                                <div className="w-full">
-                                    <div className="flex gap-1 font-semibold">
-                                        {project.name}
-                                    </div>
-                                    <div className="mt-1 text-xs text-neutral-400">
-                                        {project.description}
-                                    </div>
-                                    <div className="mt-2 text-[10px] text-neutral-300 font-light">
-                                        {project.tech}
-                                    </div>
-                                    {project.image !== undefined &&
-                                        project.image.length !== 0 && (
-                                            <img
-                                                className="mt-4 rounded-md shadow-md"
-                                                src={project.image}
-                                                alt={project.name}
-                                                width={200}
-                                                height={120}
-                                            />
-                                        )}
-                                </div>
-                            </div>
-                        </a>
-                    </Hoverable>
+                    <Project key={project.name} project={project} />
                 ))}
             </div>
         </Container>
