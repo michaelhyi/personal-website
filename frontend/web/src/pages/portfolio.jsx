877ff9ed7df87d6dbf9f8e32bf10626f23126867
Michael Yi <michaelyi2004@gmail.com>
2024-04-01 14:58:04 -0400

    feat(hackathons): added hackathon wins to portfolio section [PW-238] (#316)
    * feat(hackathons): added hackathon wins to portfolio section [PW-238]
    
    * fix(lint): fixed prettier check fails in ci

--- frontend/web/src/pages/portfolio.jsx
+++ frontend/web/src/pages/portfolio.jsx
@@ -1,6 +1,9 @@
+import { FiArrowUpRight } from "react-icons/fi";
 import BackButton from "../components/BackButton";
 import Container from "../components/Container";
+import Hoverable from "../components/Hoverable";
 import Project from "../components/Project";
+import HACKATHONS from "../constants/hackathons";
 import PROJECTS from "../constants/projects";
 
 export default function Home() {
@@ -8,8 +11,23 @@ export default function Home() {
         <Container>
             <BackButton href="/" text="Home" />
             <section className="mt-10 flex flex-col gap-8">
+                <h1 className="text-3xl font-bold">Projects</h1>
                 {PROJECTS.map((project) => (
                     <Project key={project.name} project={project} />
+                ))}
+            </section>
+            <section className="mt-16 flex flex-col gap-2">
+                <h1 className="text-3xl font-bold mb-6">Hackathons</h1>
+                {HACKATHONS.map((hackathon) => (
+                    <Hoverable key={hackathon.name}>
+                        <a
+                            className="flex text-sm font-medium"
+                            href={hackathon.href}
+                        >
+                            {hackathon.name}
+                            <FiArrowUpRight />
+                        </a>
+                    </Hoverable>
                 ))}
             </section>
         </Container>
