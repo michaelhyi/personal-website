264c45a5c1116fd8556c1b5235103ee0b3a1eac2
michaelhyi <michaelyi2004@gmail.com>
2024-04-01 14:52:38 -0400

    feat(hackathons): added hackathon wins to portfolio section [PW-238]

---
+++ frontend/web/src/constants/hackathons.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: "1st Place at HackGT X",
+        href: "https://devpost.com/software/scribeai",
+    },
+    {
+        name: "Best Access to Healthcare Hack at PennApps \"Health Hacks\"",
+        href: "https://devpost.com/software/n-a-dek950",
+    },
+    {
+        name: "2nd Place at Coding Your Future Hackathon",
+        href: "https://devpost.com/software/chestray",
+    },
+    {
+        name: "Best Environmental Hack at Unlock Hacks",
+        href: "https://devpost.com/software/leafx",
+    },
+    {
+        name: "3rd Place at CA45 Congressional App Challenge",
+        href: "https://github.com/michaelhyi/carcam",
+    },
+];
