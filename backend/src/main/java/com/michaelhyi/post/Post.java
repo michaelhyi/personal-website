a6b4d84d788c93f81ca51174effdcdce7e55b282
michaelhyi <michaelyi2004@gmail.com>
2024-04-16 12:24:17 -0400

    fix(it): fixed it failures

--- backend/src/main/java/com/michaelhyi/post/Post.java
+++ backend/src/main/java/com/michaelhyi/post/Post.java
@@ -10,9 +10,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Post implements Serializable {
     @Id
