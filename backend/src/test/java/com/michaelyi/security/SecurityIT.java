c9bc778726bc0c0043b29f2c3b79f949c04d0b4b
michaelhyi <michaelyi2004@gmail.com>
2024-05-30 20:31:48 -0500

    fix(ci): fixed test cases failing due to uninitialized testcontainers

--- backend/src/test/java/com/michaelyi/security/SecurityIT.java
+++ backend/src/test/java/com/michaelyi/security/SecurityIT.java
@@ -9,20 +9,52 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @TestPropertySource("classpath:application-it.properties")
 public class SecurityIT {
+    private static final int REDIS_PORT = 6379;
+    private static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.36");
+    private static GenericContainer<?> redis = new GenericContainer<>("redis:6.2.14")
+            .withExposedPorts(REDIS_PORT);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", () -> String.valueOf(redis.getMappedPort(REDIS_PORT)));
+    }
+
     @Autowired
     private MockMvc mvc;
     private static final String NOT_FOUND = "Not Found";
+
+    @BeforeAll
+    static void beforeAll() {
+        mysql.start();
+        redis.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mysql.stop();
+        redis.stop();
+    }
 
     @Test
     void testError() throws Exception {
