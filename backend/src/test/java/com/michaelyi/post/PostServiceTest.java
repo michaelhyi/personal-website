02a193f3a2f1622e7b15821232070ee8daeb1440
michaelhyi <michaelyi2004@gmail.com>
2024-06-06 15:56:28 -0500

    refactor(checkstyle): fixed checkstyle violations in test files

--- backend/src/test/java/com/michaelyi/post/PostServiceTest.java
+++ backend/src/test/java/com/michaelyi/post/PostServiceTest.java
@@ -1,5 +1,20 @@
 package com.michaelyi.post;
 
+import com.michaelyi.s3.S3Service;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Sort;
+import org.springframework.mock.web.MockMultipartFile;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+
+import java.util.Date;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -9,23 +24,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-import java.util.Date;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Sort;
-import org.springframework.mock.web.MockMultipartFile;
-
-import com.michaelyi.s3.S3Service;
-
-import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
-
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
     @Mock
@@ -34,7 +32,12 @@ class PostServiceTest {
     @Mock
     private S3Service s3Service;
     private PostService underTest;
-    private static final Post POST = new Post("title", new Date(), "title", "content");
+    private static final Post POST = new Post(
+            "title",
+            new Date(),
+            "title",
+            "content"
+    );
 
     @BeforeEach
     void setUp() {
@@ -43,11 +46,26 @@ class PostServiceTest {
 
     @Test
     void willThrowPostConstructorWhenBadRequest() {
-        assertThrows(IllegalArgumentException.class, () -> new Post(null));
-        assertThrows(IllegalArgumentException.class, () -> new Post(""));
-        assertThrows(IllegalArgumentException.class, () -> new Post("no-title"));
-        assertThrows(IllegalArgumentException.class, () -> new Post("<h1></h1>"));
-        assertThrows(IllegalArgumentException.class, () -> new Post("<h1>no-content</h1>"));
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Post(null)
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Post("")
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Post("no-title")
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Post("<h1></h1>")
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Post("<h1>no-content</h1>")
+        );
 
         String text = "<h1>title (1994)</h1>content";
         Post newPost = new Post(text);
@@ -64,7 +82,10 @@ class PostServiceTest {
         text = "<h1>It's Such A Beautiful Day (2012)</h1>Don Hertzfeldt";
         newPost = new Post(text);
         assertEquals("its-such-a-beautiful-day", newPost.getId());
-        assertEquals("It's Such A Beautiful Day (2012)", newPost.getTitle());
+        assertEquals(
+                "It's Such A Beautiful Day (2012)",
+                newPost.getTitle()
+        );
         assertEquals("Don Hertzfeldt", newPost.getContent());
     }
 
@@ -73,7 +94,10 @@ class PostServiceTest {
         when(repository.findById("title")).thenReturn(Optional.of(POST));
 
         assertThrows(IllegalArgumentException.class, () ->
-                underTest.createPost("<h1>title (1994)</h1><p>content</p>", null));
+                underTest.createPost(
+                        "<h1>title (1994)</h1><p>content</p>",
+                        null)
+        );
         verify(repository).findById("title");
         verifyNoMoreInteractions(repository);
     }
@@ -81,21 +105,29 @@ class PostServiceTest {
     @Test
     void willThrowCreatePostWhenImageDoesNotExist() {
         assertThrows(IllegalArgumentException.class, () ->
-                underTest.createPost("<h1>title (1994)</h1><p>content</p>", null));
+                underTest.createPost(
+                        "<h1>title (1994)</h1><p>content</p>",
+                        null
+                )
+        );
         verify(repository).findById("title");
         verifyNoMoreInteractions(repository);
     }
 
     @Test
     void createPost() {
-        when(repository.findById(POST.getId())).thenReturn(Optional.empty());
+        when(repository.findById(POST.getId())).thenReturn(
+                Optional.empty()
+        );
 
         String actualId = underTest.createPost(
                 "<h1>title (1994)</h1>content",
                 new MockMultipartFile("image", "Hello World!".getBytes())
         );
 
-        ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(Post.class);
+        ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(
+                Post.class
+        );
 
         verify(repository).findById(POST.getId());
         verify(repository).saveAndFlush(postArgumentCaptor.capture());
@@ -112,7 +144,10 @@ class PostServiceTest {
     void willThrowReadPostWhenPostNotFound() {
         when(repository.findById("title")).thenReturn(Optional.empty());
 
-        assertThrows(NoSuchElementException.class, () -> underTest.readPost("title"));
+        assertThrows(
+                NoSuchElementException.class,
+                () -> underTest.readPost("title")
+        );
         verify(repository).findById("title");
     }
 
@@ -132,7 +167,10 @@ class PostServiceTest {
     void willThrowReadPostImageWhenPostNotFound() {
         when(repository.findById("title")).thenReturn(Optional.empty());
 
-        assertThrows(NoSuchElementException.class, () -> underTest.readPostImage("title"));
+        assertThrows(
+                NoSuchElementException.class,
+                () -> underTest.readPostImage("title")
+        );
         verify(repository).findById("title");
         verify(s3Service, never()).getObject(any());
     }
@@ -140,9 +178,13 @@ class PostServiceTest {
     @Test
     void willThrowReadPostImageWhenS3KeyNotFound() {
         when(repository.findById("title")).thenReturn(Optional.of(POST));
-        when(s3Service.getObject("title")).thenThrow(NoSuchKeyException.class);
+        when(s3Service.getObject("title"))
+                .thenThrow(NoSuchKeyException.class);
 
-        assertThrows(NoSuchElementException.class, () -> underTest.readPostImage("title"));
+        assertThrows(
+                NoSuchElementException.class,
+                () -> underTest.readPostImage("title")
+        );
         verify(repository).findById("title");
         verify(s3Service).getObject("title");
     }
@@ -164,7 +206,9 @@ class PostServiceTest {
     @Test
     void readAllPosts() {
         underTest.readAllPosts();
-        verify(repository).findAll(Sort.by(Sort.Direction.DESC, "date"));
+        verify(repository).findAll(
+                Sort.by(Sort.Direction.DESC, "date")
+        );
     }
 
     @Test
@@ -172,7 +216,11 @@ class PostServiceTest {
         when(repository.findById("title")).thenReturn(Optional.empty());
 
         assertThrows(NoSuchElementException.class,
-                () -> underTest.updatePost("title", "<h1>title (1994)</h1>content", null)
+                () -> underTest.updatePost(
+                        "title",
+                        "<h1>title (1994)</h1>content",
+                        null
+                )
         );
 
         verify(repository).findById("title");
@@ -183,9 +231,14 @@ class PostServiceTest {
     void updatePost() {
         when(repository.findById("title")).thenReturn(Optional.of(POST));
 
-        underTest.updatePost("title", "<h1>title (1994)</h1>content", null);
+        underTest.updatePost(
+                "title",
+                "<h1>title (1994)</h1>content",
+                null
+        );
 
-        ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(Post.class);
+        ArgumentCaptor<Post> postArgumentCaptor =
+                ArgumentCaptor.forClass(Post.class);
 
         verify(repository, times(2)).findById("title");
         verify(repository).save(postArgumentCaptor.capture());
@@ -196,9 +249,17 @@ class PostServiceTest {
     void updatePostImage() {
         when(repository.findById("title")).thenReturn(Optional.of(POST));
 
-        underTest.updatePost("title", "<h1>title (1994)</h1>content", new MockMultipartFile("image", "New Hello World!".getBytes()));
+        underTest.updatePost(
+                "title",
+                "<h1>title (1994)</h1>content",
+                new MockMultipartFile(
+                        "image",
+                        "New Hello World!".getBytes()
+                )
+        );
 
-        ArgumentCaptor<Post> postArgumentCaptor = ArgumentCaptor.forClass(Post.class);
+        ArgumentCaptor<Post> postArgumentCaptor =
+                ArgumentCaptor.forClass(Post.class);
 
         verify(repository, times(2)).findById("title");
         verify(repository).save(postArgumentCaptor.capture());
@@ -211,7 +272,10 @@ class PostServiceTest {
     void willThrowDeletePostWhenPostNotFound() {
         when(repository.findById("title")).thenReturn(Optional.empty());
 
-        assertThrows(NoSuchElementException.class, () -> underTest.deletePost("title"));
+        assertThrows(
+                NoSuchElementException.class,
+                () -> underTest.deletePost("title")
+        );
         verifyNoMoreInteractions(s3Service);
         verifyNoMoreInteractions(repository);
     }
