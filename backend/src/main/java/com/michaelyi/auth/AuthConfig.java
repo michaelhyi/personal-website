ec5db31d2aa1c9fb4a9eba461e66b90687df9c27
michaelhyi <michaelyi2004@gmail.com>
2024-06-10 12:37:24 -0500

    fix(checkstyle): fixed checkstyle violations

--- backend/src/main/java/com/michaelyi/auth/AuthConfig.java
+++ backend/src/main/java/com/michaelyi/auth/AuthConfig.java
@@ -1,7 +1,6 @@
 package com.michaelyi.auth;
 
 import com.michaelyi.user.User;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
