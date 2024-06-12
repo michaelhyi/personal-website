e40dffdef5126a46ff3acc4282d9880c0d1d118e
michaelhyi <michaelyi2004@gmail.com>
2024-06-12 11:20:15 -0500

    refactor(deps): removed framer-motion [PW-265]

--- frontend/web/tailwind.config.js
+++ frontend/web/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
+            animation: {
+                fadeIn: "fadeIn 1s ease-in-out",
+            },
+            keyframes: {
+                fadeIn: {
+                    "0%": { opacity: 0 },
+                    "100%": { opacity: 1 },
+                },
+            },
             screens: {
                 sm: "320px",
             },
