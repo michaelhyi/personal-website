92a81f178bf518e1314778b7e7dced792515a40d
michaelhyi <michaelyi2004@gmail.com>
2024-06-22 01:25:50 -0500

    fix(checkstyle): fixed checkstyle violations

--- backend/src/main/java/com/michaelyi/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/michaelyi/security/JwtAuthenticationFilter.java
@@ -31,7 +31,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        if (request.getServletPath().contains(String.format("/%s/auth", CONTEXT_PATH))) {
+        if (request
+                .getServletPath()
+                .contains(
+                        String.format("/%s/auth", CONTEXT_PATH)
+                )
+        ) {
             filterChain.doFilter(request, response);
             return;
         }
