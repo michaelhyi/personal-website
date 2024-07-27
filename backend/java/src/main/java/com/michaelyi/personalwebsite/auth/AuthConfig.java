2ae7e904e29be01fbe7d63807007627bb32790d8
michaelhyi <michaelyi2004@gmail.com>
2024-07-27 12:34:33 -0500

    feat(security): fixed dependency injection of admin password [PW-305]

--- backend/java/src/main/java/com/michaelyi/personalwebsite/auth/AuthConfig.java
+++ backend/java/src/main/java/com/michaelyi/personalwebsite/auth/AuthConfig.java
@@ -1,15 +1,21 @@
 package com.michaelyi.personalwebsite.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static com.michaelyi.personalwebsite.util.Constants.SECURITY_AUTH_ADMIN_PW;
+
 @Configuration
 public class AuthConfig {
     private final String adminPassword;
 
-    public AuthConfig(String adminPassword) {
+    public AuthConfig(
+            @Value(SECURITY_AUTH_ADMIN_PW)
+            String adminPassword
+    ) {
         this.adminPassword = adminPassword;
     }
 
