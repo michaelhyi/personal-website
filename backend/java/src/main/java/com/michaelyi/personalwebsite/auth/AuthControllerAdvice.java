6e200c4ed2d42bdfbb41ef7e8e9c67aca48da184
michaelhyi <michaelyi2004@gmail.com>
2024-07-27 13:05:51 -0500

    feat(security): fixed test failures [PW-305]

--- backend/java/src/main/java/com/michaelyi/personalwebsite/auth/AuthControllerAdvice.java
+++ backend/java/src/main/java/com/michaelyi/personalwebsite/auth/AuthControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.michaelyi.personalwebsite.auth;
 
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class AuthControllerAdvice {
     @ExceptionHandler({
             JwtException.class,
-            UnauthorizedException.class
+            UnauthorizedException.class,
+            SignatureException.class
     })
-    public ResponseEntity<String> handleUnauthorizedException(
-            RuntimeException e
-    ) {
-        return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<String> handleUnauthorizedException() {
+        return new ResponseEntity<>(
+                "Unauthorized",
+                HttpStatus.UNAUTHORIZED
+        );
     }
 }
