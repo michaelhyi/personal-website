e80c65c477bc7c9f27aed9feedc54ab48304a758
michaelhyi <michaelyi2004@gmail.com>
2024-07-27 19:23:02 -0500

    feat(health): fixed health it failures [PW-313]

--- backend/java/src/test/java/com/michaelyi/personalwebsite/health/HealthIT.java
+++ backend/java/src/test/java/com/michaelyi/personalwebsite/health/HealthIT.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @TestPropertySource("classpath:application-test.properties")
 public class HealthIT extends IntegrationTest {
     @Autowired
