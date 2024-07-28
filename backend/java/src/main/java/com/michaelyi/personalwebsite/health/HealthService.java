3d6a07fbc1827532991f4b8e6c07cd01bb1f787a
michaelhyi <michaelyi2004@gmail.com>
2024-07-27 19:12:12 -0500

    fix(checkstyle): fixed checkstyle violations

--- backend/java/src/main/java/com/michaelyi/personalwebsite/health/HealthService.java
+++ backend/java/src/main/java/com/michaelyi/personalwebsite/health/HealthService.java
@@ -7,15 +7,21 @@ import java.util.Date;
 @Service
 public class HealthService {
     private static final Date START_TIME = new Date();
+    private static final int MINUTES_PER_HOUR = 60;
+    private static final int SECONDS_PER_MINUTE = 60;
+    private static final int MS_PER_SECOND = 1000;
 
     public HealthResponse check() {
         Date timestamp = new Date();
         long uptime = timestamp.getTime() - START_TIME.getTime();
 
-        long hrs = (uptime / (1000 * 60 * 60)) % 24;
-        long mins = (uptime / (1000 * 60)) % 60;
-        long seconds = (uptime / 1000) % 60;
-        long ms = uptime % 1000;
+        long hrs = (
+                uptime / (MS_PER_SECOND * SECONDS_PER_MINUTE * MINUTES_PER_HOUR)
+        );
+        long mins = (uptime / (MS_PER_SECOND * SECONDS_PER_MINUTE))
+                % MINUTES_PER_HOUR;
+        long seconds = (uptime / MS_PER_SECOND) % SECONDS_PER_MINUTE;
+        long ms = uptime % MS_PER_SECOND;
 
         String formattedUptime = String.format(
                 "%02dh %02dm %02ds %dms", hrs, mins, seconds, ms
