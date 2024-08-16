10840ab266ca3e3c6cb605338e1058de87d3e390
michaelhyi <michaelyi2004@gmail.com>
2024-08-16 00:14:51 -0700

    feat(update-post): changed http method for updates to patch from put [PW-331]

--- backend/java/src/main/java/com/michaelyi/personalwebsite/post/PostController.java
+++ backend/java/src/main/java/com/michaelyi/personalwebsite/post/PostController.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -111,7 +111,7 @@ public class PostController {
         return new ResponseEntity<>(res, res.getHttpStatus());
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<HttpResponse> updatePost(
             @PathVariable String id,
             @RequestParam("text") String text,
