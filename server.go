2a880054edd3d2ea1140edcee1095a3da591407c
michaelhyi <michaelyi2004@gmail.com>
2024-10-25 01:09:38 -0400

    feat: implemented new ui designs

---
+++ server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	pages := map[string]string{
+		"/":          "./index.html",
+		"/about":     "./pages/about.html",
+		"/lauren":    "./pages/lauren.html",
+		"/portfolio": "./pages/portfolio.html",
+	}
+
+	for route, file := range pages {
+		http.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, file)
+		})
+	}
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	log.Printf("Server started on port 3000")
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
