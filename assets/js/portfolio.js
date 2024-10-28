4c348017397c4d27be1d61656c6e855f7ef52b2e
Michael Yi <michaelyi2004@gmail.com>
2024-10-27 22:23:51 -0400

    feat: removed personal website link from portfolio (#479)

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,8 +3,7 @@ const PROJECTS = [
         name: "Personal Website",
         date: "Oct 2022 - Oct 2024",
         description: "",
-        tech: "HTML, CSS, JS, Go",
-        href: "https://michael-yi.com",
+        tech: "HTML, CSS, JS, Go"
     },
     {
         name: "ScribeAI (1st Place Overall at HackGT)",
@@ -13,21 +12,21 @@ const PROJECTS = [
             "A HIPAA-compliant, decentralized healthcare management platform that uses AI to transform physical healthcare documents into electronic health records.",
         tech: "Java, Spring, PostgreSQL, Docker, Kubernetes, TypeScript, Next.js, Tailwind, Tesseract, OpenAI",
         image: "/assets/img/scribeai.png",
-        href: "https://github.com/michaelhyi/scribeai",
+        href: "https://github.com/michaelhyi/scribeai"
     },
     {
         name: "Sales Invoice Automation Platform",
         date: "Apr - Jun 2024",
         description:
             "A platform for a logistics company that generates and bills invoices from Excel sales data.",
-        tech: "Java, Spring, gRPC, Kafka, GraphQL, MySQL, AWS S3, Docker, React, Tailwind",
+        tech: "Java, Spring, gRPC, Kafka, GraphQL, MySQL, AWS S3, Docker, React, Tailwind"
     },
     {
         name: "Film Blog",
         date: "Aug 2023 - Oct 2024",
         description: "A showcase of my passion for cinema.",
         tech: "Java, Go, Spring, MySQL, Redis, JUnit, React",
-        href: "https://film.michael-yi.com/",
+        href: "https://film.michael-yi.com/"
     },
     {
         name: "health{hacks} Website",
@@ -35,7 +34,7 @@ const PROJECTS = [
         description: "Assembling the future innovators of medicine.",
         tech: "TypeScript, Next.js, Tailwind, Node.js, Express.js, PostgreSQL, MongoDB, GraphQL",
         image: "/assets/img/hhacks.png",
-        href: "https://www.joinhealthhacks.com/",
+        href: "https://www.joinhealthhacks.com/"
     },
     {
         name: "WeVote",
@@ -51,13 +50,13 @@ const PROJECTS = [
             "An NLP + Vision model that generates coherent sentences given respective keywords (concepts) and a coresponding image.",
         tech: "Python, PyTorch",
         image: "/assets/img/shine.png",
-        href: "https://github.com/michaelhyi/usc-shine",
+        href: "https://github.com/michaelhyi/usc-shine"
     },
     {
         name: "Linkix",
         date: "Apr 2022",
         description: "A social media platform tailored for pre-professional students aiming to connect and collaborate.",
-        tech: "TypeScript, Python, React Native, Flask, MongoDB",
+        tech: "TypeScript, Python, React Native, Flask, MongoDB"
     },
     {
         name: "MelaModel",
@@ -65,7 +64,7 @@ const PROJECTS = [
         description:
             "Best Access to Healthcare Hack @ PennApps Health Hacks. An image-recognition software that differentiates between benign and malignant skin moles using Artificial Intelligence.",
         tech: "Python, Tensorflow, Keras, TypeScript, React Native, Node.js, AWS Lambda, AWS DynamoDB, AWS CloudWatch",
-        href: "https://github.com/michaelhyi/melamodel",
+        href: "https://github.com/michaelhyi/melamodel"
     },
     {
         name: "ChestRay",
@@ -73,7 +72,7 @@ const PROJECTS = [
         description:
             "2nd Place @ Coding Your Future Hackathon. A powerful bridge between doctors, patients, and Artificial Intelligence based x-ray diagnosis.",
         tech: "Python, JavaScript, Tensorflow, Keras, React Native, Firebase",
-        href: "https://github.com/michaelhyi/chestray",
+        href: "https://github.com/michaelhyi/chestray"
     },
     {
         name: "LeafX",
@@ -81,7 +80,7 @@ const PROJECTS = [
         description:
             "Best Environmental Hack @ Unlock Hacks. A powerful medical tool for plant health.",
         tech: "Python, JavaScript, Tensorflow, Keras, React Native",
-        href: "https://github.com/michaelhyi/leafx",
+        href: "https://github.com/michaelhyi/leafx"
     },
     {
         name: "HealthRelay",
@@ -90,7 +89,7 @@ const PROJECTS = [
             "Streamlining secure communication between radiologists and ordering physicians.",
         tech: "TypeScript, React Native, PostgreSQL, GraphQL",
         image: "/assets/img/healthrelay.png",
-        href: "https://healthrelay.vercel.app/",
+        href: "https://healthrelay.vercel.app/"
     },
     {
         name: "RetinaOCT",
@@ -99,7 +98,7 @@ const PROJECTS = [
             "Enhancing Retinal Abnormality Detection through Deep-Learning-based Optical Coherence Tomography Analysis",
         tech: "Python, Tensorflow, Keras, TypeScript, React Native, PostgreSQL, GraphQL",
         image: "/assets/img/retinaoct.png",
-        href: "https://github.com/michaelhyi/retinaoct",
+        href: "https://github.com/michaelhyi/retinaoct"
     },
     {
         name: "CarCam",
@@ -107,36 +106,36 @@ const PROJECTS = [
         description:
             "3rd Place @ 2021 Congressional App Challenge CA45 District. Bridging the digital divide by providing powerful dashcam technology to your phone.",
         tech: "JavaScript, React Native",
-        href: "https://github.com/michaelhyi/carcam",
+        href: "https://github.com/michaelhyi/carcam"
     },
     {
         name: "Medixly",
         date: "Jan 2021 - Oct 2022",
         description: "A mobile application that uses machine learning to diagnose skin cancer through images.",
-        tech: "JavaScript, React Native",
+        tech: "JavaScript, React Native"
     }
 ];
 
 const HACKATHONS = [
     {
         name: "1st Place Overall at HackGT X",
-        href: "https://devpost.com/software/scribeai",
+        href: "https://devpost.com/software/scribeai"
     },
     {
         name: `Best Access to Healthcare Hack at PennApps "Health Hacks"`,
-        href: "https://devpost.com/software/n-a-dek950",
+        href: "https://devpost.com/software/n-a-dek950"
     },
     {
         name: "2nd Place at Coding Your Future Hackathon",
-        href: "https://devpost.com/software/chestray",
+        href: "https://devpost.com/software/chestray"
     },
     {
         name: "Best Environmental Hack at Unlock Hacks",
-        href: "https://devpost.com/software/leafx",
+        href: "https://devpost.com/software/leafx"
     },
     {
         name: "3rd Place at CA45 Congressional App Challenge",
-        href: "https://www.youtube.com/watch?v=oFE0Inj-pr8",
+        href: "https://www.youtube.com/watch?v=oFE0Inj-pr8"
     }
 ];
 
