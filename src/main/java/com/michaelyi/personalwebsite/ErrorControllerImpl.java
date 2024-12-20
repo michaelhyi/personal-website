18fadd6e69869fba87089094e36fc5fbcdb19369
michaelhyi <michaelyi2004@gmail.com>
2024-12-19 19:33:50 -0800

    fix: fix error page class

--- src/main/java/com/michaelyi/personalwebsite/ErrorControllerImpl.java
+++ src/main/java/com/michaelyi/personalwebsite/ErrorControllerImpl.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class NotFoundController implements ErrorController {
+public class ErrorControllerImpl implements ErrorController {
     @GetMapping("/error")
     public String error() {
         return "404 page not found";
