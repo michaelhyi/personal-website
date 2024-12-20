d51c76f5acdcf8862ca7f06b9e35c06fdb4c6979
michaelhyi <michaelyi2004@gmail.com>
2024-12-19 19:45:46 -0800

    feat: added http status code for error endpoint

--- src/main/java/com/michaelyi/personalwebsite/ErrorControllerImpl.java
+++ src/main/java/com/michaelyi/personalwebsite/ErrorControllerImpl.java
@@ -1,13 +1,15 @@
 package com.michaelyi.personalwebsite;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ErrorControllerImpl implements ErrorController {
     @GetMapping("/error")
-    public String error() {
-        return "404 page not found";
+    public ResponseEntity<String> error() {
+        return new ResponseEntity<>("404 page not found", HttpStatus.NOT_FOUND);
     }
 }
